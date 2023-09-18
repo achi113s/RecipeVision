afb4eb568fc5e2a7c34cbd9d38ab53381cbe1bfa
achi113s <latourgiorgio@gmail.com>
2023-09-17 22:49:25 -0400

    got chatgpt working amended

---
+++ RecipeVision/Enums/ChatGPT.swift
@@ -0,0 +1,12 @@
+//
+//  ChatGPT.swift
+//  RecipeVision
+//
+//  Created by Giorgio Latour on 9/17/23.
+//
+
+import Foundation
+
+enum Chat: String {
+    case chatgpt = "gpt-3.5-turbo"
+}
--- RecipeVision/ViewModels/VisionViewModel.swift
+++ RecipeVision/ViewModels/VisionViewModel.swift
@@ -12,6 +12,14 @@ class VisionViewModel: NSObject, ObservableObject {
     @Published var imageToProcess: UIImage? = nil
     @Published var roi: CGRect? = nil
     
+    @Published var openAIViewModel = OpenAIViewModel()
+    
+    //    @State private var ingredients: String? = nil {
+    //        didSet {
+    //            print("set ingredients to: \(ingredients)")
+    //        }
+    //    }
+    
     public func setImageToProcess(_ image: UIImage, roi: CGRect) {
         imageToProcess = image
         self.roi = roi
@@ -71,7 +79,6 @@ class VisionViewModel: NSObject, ObservableObject {
             return (observation.boundingBox, observation.topCandidates(1)[0].string)
         }
         
-//        let recognizedItems = recognizeIngredientsByLineSpacing(results)
         let recognizedItems = results.map { result in
             return result.1
         }
@@ -84,88 +91,101 @@ class VisionViewModel: NSObject, ObservableObject {
         print(recognizedItems.joined(separator: " "))
         print(recognizedStringsBoundingBoxes)
         
-//        DispatchQueue.main.async {
-//            print("Current Thread: \(Thread.current). Should be main.")
-//            recipeItems.recipeItems = recognizedItems
-//            recognizedTextBoundingBoxes = recognizedStringsBoundingBoxes
-//            withAnimation(.easeInOut(duration: 1)) {
-//                requestInProgress = false
-//            }
-//        }
+        let ingredients = recognizedItems.joined(separator: " ")
+        
+        Task {
+            print("getopenaicompletion called")
+            let content = """
+             Separate this ingredient list into an array of strings of its
+             components and output as a JSON object. The JSON object should
+             have the following format: {"ingredients": []}.
+             Here are the ingredients: \"\(ingredients)\"
+            """
+            try await openAIViewModel.postMessage(content: content, role: "system", model: Chat.chatgpt.rawValue)
+        }
+        
+        //        DispatchQueue.main.async {
+        //            print("Current Thread: \(Thread.current). Should be main.")
+        //            recipeItems.recipeItems = recognizedItems
+        //            recognizedTextBoundingBoxes = recognizedStringsBoundingBoxes
+        //            withAnimation(.easeInOut(duration: 1)) {
+        //                requestInProgress = false
+        //            }
+        //        }
     }
     
     private func myProgressHandler(request: VNRequest, progress: Double, error: Error?) {
         print(progress)
     }
     
-//    private func recognizeIngredientsByLineSpacing(_ observations: [(CGRect, String)]) -> [RecipeItem] {
-//        /* Process the results from the VNRecognizeTextRequest based on the spatial
-//         relationship between the observations. This does not generalize well. In this
-//         case, we will consider the situation where ingredients are wrapped on new lines
-//         and each is separated by a blank new line. Hence, if the spacing between one observation
-//         and the next is small, means those two lines are part of the same ingredient.
-//         */
-//        print("Current Thread: \(Thread.current). Should not be main")
-//        let lineHeightThreshold = 0.8
-//
-//        // Find the maximum and minimum space between observations.
-//        var minSpacing: CGFloat = .infinity  // Presumed to be the line spacing.
-//        var maxSpacing: CGFloat = -.infinity  // Presumed to be the paragraph spacing.
-//        for index in stride(from: 0, to: observations.count - 1, by: 1) {
-//            let firstObservation = observations[index]
-//
-//            if index + 1 < observations.count {
-//                let secondObservation = observations[index + 1]
-//
-//                let verticalSpacing = firstObservation.0.origin.y - secondObservation.0.origin.y
-//
-//                if verticalSpacing < minSpacing { minSpacing = verticalSpacing }
-//                if verticalSpacing >= maxSpacing { maxSpacing = verticalSpacing }
-//            }
-//        }
-//
-////        print("The minimum space between observations is \(minSpacing).")
-////        print("The maximum line spacing is \(maxSpacing).")
-//
-//        var groupedObservedStrings = [RecipeItem]()
-//        var buildIngredient = observations[0].1  // Start building on the first observation.
-//
-//        for index in stride(from: 0, to: observations.count - 1, by: 1) {
-//            let firstObservation = observations[index]
-//
-//            // Make sure we don't go out of index bounds.
-//            if index + 1 < observations.count {
-//                let secondObservation = observations[index + 1]
-//
-//                let verticalSpacing = firstObservation.0.origin.y - secondObservation.0.origin.y
-////                print("The vertical spacing between \(firstObservation.1) and \(secondObservation.1) is \(verticalSpacing).")
-//
-//                // If the spacing between observations is less than lineHeightThreshold times the maximum spacing,
-//                // it's likely this is part of one ingredient.
-//                if verticalSpacing < (maxSpacing * lineHeightThreshold) {
-////                    print("\(firstObservation.1) and \(secondObservation.1) are part of the same ingredient.")
-//                    buildIngredient = "\(buildIngredient) \(secondObservation.1)"
-////                    print(buildIngredient)
-//                } else {
-//                    // The buildIngredient will be blank if this is the beginning of an ingredient after
-//                    // a blank line.
-//                    if !buildIngredient.isEmpty {
-//                        let recipeItem = RecipeItem(name: buildIngredient)
-//                        groupedObservedStrings.append(recipeItem)
-//                        buildIngredient = secondObservation.1
-//                    } else {
-//                        let recipeItem = RecipeItem(name: secondObservation.1)
-//                        groupedObservedStrings.append(recipeItem)
-//                    }
-//                }
-//            }
-//        }
-//
-//        if !buildIngredient.isEmpty {
-//            let recipeItem = RecipeItem(name: buildIngredient)
-//            groupedObservedStrings.append(recipeItem)
-//        }
-//
-//        return groupedObservedStrings
-//    }
+    //    private func recognizeIngredientsByLineSpacing(_ observations: [(CGRect, String)]) -> [RecipeItem] {
+    //        /* Process the results from the VNRecognizeTextRequest based on the spatial
+    //         relationship between the observations. This does not generalize well. In this
+    //         case, we will consider the situation where ingredients are wrapped on new lines
+    //         and each is separated by a blank new line. Hence, if the spacing between one observation
+    //         and the next is small, means those two lines are part of the same ingredient.
+    //         */
+    //        print("Current Thread: \(Thread.current). Should not be main")
+    //        let lineHeightThreshold = 0.8
+    //
+    //        // Find the maximum and minimum space between observations.
+    //        var minSpacing: CGFloat = .infinity  // Presumed to be the line spacing.
+    //        var maxSpacing: CGFloat = -.infinity  // Presumed to be the paragraph spacing.
+    //        for index in stride(from: 0, to: observations.count - 1, by: 1) {
+    //            let firstObservation = observations[index]
+    //
+    //            if index + 1 < observations.count {
+    //                let secondObservation = observations[index + 1]
+    //
+    //                let verticalSpacing = firstObservation.0.origin.y - secondObservation.0.origin.y
+    //
+    //                if verticalSpacing < minSpacing { minSpacing = verticalSpacing }
+    //                if verticalSpacing >= maxSpacing { maxSpacing = verticalSpacing }
+    //            }
+    //        }
+    //
+    ////        print("The minimum space between observations is \(minSpacing).")
+    ////        print("The maximum line spacing is \(maxSpacing).")
+    //
+    //        var groupedObservedStrings = [RecipeItem]()
+    //        var buildIngredient = observations[0].1  // Start building on the first observation.
+    //
+    //        for index in stride(from: 0, to: observations.count - 1, by: 1) {
+    //            let firstObservation = observations[index]
+    //
+    //            // Make sure we don't go out of index bounds.
+    //            if index + 1 < observations.count {
+    //                let secondObservation = observations[index + 1]
+    //
+    //                let verticalSpacing = firstObservation.0.origin.y - secondObservation.0.origin.y
+    ////                print("The vertical spacing between \(firstObservation.1) and \(secondObservation.1) is \(verticalSpacing).")
+    //
+    //                // If the spacing between observations is less than lineHeightThreshold times the maximum spacing,
+    //                // it's likely this is part of one ingredient.
+    //                if verticalSpacing < (maxSpacing * lineHeightThreshold) {
+    ////                    print("\(firstObservation.1) and \(secondObservation.1) are part of the same ingredient.")
+    //                    buildIngredient = "\(buildIngredient) \(secondObservation.1)"
+    ////                    print(buildIngredient)
+    //                } else {
+    //                    // The buildIngredient will be blank if this is the beginning of an ingredient after
+    //                    // a blank line.
+    //                    if !buildIngredient.isEmpty {
+    //                        let recipeItem = RecipeItem(name: buildIngredient)
+    //                        groupedObservedStrings.append(recipeItem)
+    //                        buildIngredient = secondObservation.1
+    //                    } else {
+    //                        let recipeItem = RecipeItem(name: secondObservation.1)
+    //                        groupedObservedStrings.append(recipeItem)
+    //                    }
+    //                }
+    //            }
+    //        }
+    //
+    //        if !buildIngredient.isEmpty {
+    //            let recipeItem = RecipeItem(name: buildIngredient)
+    //            groupedObservedStrings.append(recipeItem)
+    //        }
+    //
+    //        return groupedObservedStrings
+    //    }
 }
